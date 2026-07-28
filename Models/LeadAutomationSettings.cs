be4830c1bec6722a36c88deac3fd7f4da5fc4ddf
Istiak Ahmed <22312104@iubat.edu>
2026-07-28 12:48:33 +0600

    Support fractional lead automation hours

--- Models/LeadAutomationSettings.cs
+++ Models/LeadAutomationSettings.cs
@@ -3,7 +3,7 @@ namespace backend.Models;
 public class LeadAutomationSettings
 {
     public int Id { get; set; } = 1;
-    public int UnassignAfterHours { get; set; } = 24;
-    public int ReminderIntervalHours { get; set; } = 1;
+    public decimal UnassignAfterHours { get; set; } = 24;
+    public decimal ReminderIntervalHours { get; set; } = 1;
     public DateTime UpdatedAt { get; set; } = DateTime.UtcNow;
 }
